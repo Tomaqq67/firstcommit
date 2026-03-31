05788ca3e68f0fbbc42d214cd8ef056450ab8de4
Slava <slava@mail.ru>
2026-03-31 22:14:36 +0700

    добавлена модель исполнителя

--- music/main/models.py
+++ music/main/models.py
@@ -11,5 +11,13 @@ class Track(models.Model):
     title = models.CharField(max_length = 500, unique = True)
     duration = models.IntegerField()
     genre = models.ManyToManyField(Genre)
+    artist = models.ForeignKey(Artist, on_delete=models.RESTRICT, null=True)
     def __str__(self):
         return self.title
+
+class Artist(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    image = models.ImageField(upload_to='artists/', null=True)
+     def __str__(self):
+        return self.name
+    
